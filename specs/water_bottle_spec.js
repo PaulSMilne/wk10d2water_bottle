fff2f69c283bc451401f36486616773fdd3aaaa7
Paul Milne <hallhill@gmail.com>
2016-11-29 14:09:19 +0000

    Finished the tests and methods for the water bottle

--- specs/water_bottle_spec.js
+++ specs/water_bottle_spec.js
@@ -6,3 +6,32 @@ describe('Water bottle', function(){
           assert.equal(0, bottle.volume);
      })
 });
+
+describe('Water bottle', function(){
+     it("should go to 100 when full", function(){
+          assert.equal(100, bottle.fill());
+     })
+});
+
+describe('Water bottle', function(){
+     it("drink should decrease volume by 10", function(){
+          bottle.fill();
+          bottle.drink();
+          assert.equal(80, bottle.drink());
+     })
+});
+
+describe('Water bottle', function(){
+     it("should go to 0 when empty", function(){
+          assert.equal(0, bottle.empty());
+     })
+});
+
+describe('Water bottle volume', function(){
+     it("should not go below 0", function(){
+          bottle.empty();
+          bottle.drink();
+          assert.equal(0, bottle.volume);
+     })
+});
+
