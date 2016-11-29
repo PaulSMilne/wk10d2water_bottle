2a095317fe58a088645cfb27aa648a6ba823cf83
Paul Milne <hallhill@gmail.com>
2016-11-29 13:38:58 +0000

    wrote a test for initial water level

--- specs/water_bottle_spec.js
+++ specs/water_bottle_spec.js
@@ -1,2 +1,8 @@
 var bottle = require('../water_bottle');
+var assert = require('assert');
 
+describe('Water bottle', function({
+     it('should be empty at start', function(){
+          assert.equals(0, bottle.volume);
+     })
+}));
